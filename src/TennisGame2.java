1692109ccdbb817fb4dbef9e3527aac326dc2040
Juan5757 <52605634+Juan5757@users.noreply.github.com>
2020-02-17 08:10:50 -0400

    Refactorización en normal2 - eliminación de variables innecesarias

--- src/TennisGame2.java
+++ src/TennisGame2.java
@@ -76,9 +76,7 @@ public class TennisGame2 implements TennisGame
 	private String normal2(String score) {
 		if (P1point>P2point && P1point < 4)
         {
-			P1res=getLiteral(P1point);
-            P2res=getLiteral(P2point);
-            score = P1res + "-" + P2res;
+            score = getLiteral(P1point) + "-" + getLiteral(P2point);
         }
 		return score;
 	}
